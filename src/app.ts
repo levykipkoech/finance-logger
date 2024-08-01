fd95c6ebfee9cf1b5debb04cc351f66fcadd19d8
levy kipkoech <kipkoechlevy096@gmail.com>
2024-08-01 16:16:34 +0300

    created a classes folder

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-const form = document.querySelectorAll('form')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+  console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
---
+++ src/classes/invoice.ts
@@ -0,0 +1,10 @@
+class invoice{
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number
+    ){}
+    format(){
+        return`${this.client} owes ${this.amount} for ${this.details}`
+    }
+}
